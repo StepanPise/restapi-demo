2f675512273c8706efff034a538b208dd6a75a66
stepanPise <stepan.pise@gmail.com>
2025-11-25 21:01:01 +0100

    wip: migrate to PostgreSQL with Docker - updated UserService

--- src/main/java/com/example/springbootdemo/service/UserService.java
+++ src/main/java/com/example/springbootdemo/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.springbootdemo.service;
 import com.example.springbootdemo.api.model.User;
 import com.example.springbootdemo.dto.ResponseUserDTO;
 import com.example.springbootdemo.dto.UpdateUserDTO;
+import com.example.springbootdemo.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,62 +20,39 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public int generateID() {
-        int maxId = 0;
-        for (User u : users) {
-            if (u.getId() > maxId) {
-                maxId = u.getId();
-            }
-        }
-        return maxId + 1;
-    }
-
     public ResponseUserDTO getUser(Integer id) {
-        for (User u : users) {
-            if (u.getId() == id) {
-                ResponseUserDTO newResponseUserDTO = new ResponseUserDTO(u);
-                return newResponseUserDTO;
-            }
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        return userRepository.findById(id)
+                .map(ResponseUserDTO::new)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     public List<ResponseUserDTO> getAllUsers() {
-
-        List<ResponseUserDTO> responseList = new ArrayList<>();
-        for (User u : users) {
-            responseList.add(new ResponseUserDTO(u));
-        }
-
-        return responseList;
+        return userRepository.findAll().stream().map(ResponseUserDTO::new).toList();
     }
 
     public User addUser(User user) {
-        users.add(user);
-        return user;
+        return userRepository.save(user);
     }
 
     public void deleteUser(Integer id) {
-        for (User u : users) {
-            if (u.getId() == id) {
-                users.remove(u);
-                return;
-            }
+        if (!userRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        userRepository.deleteById(id);
     }
 
     public User updateUser(Integer id, UpdateUserDTO updateUserDTO) {
-        for (int i = 0; i < users.size(); i++) {
-            if(users.get(i).getId() == id) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 
-                User newUser = new User(id,updateUserDTO);
+        existingUser.setName(updateUserDTO.getName());
+        existingUser.setAge(updateUserDTO.getAge());
+        existingUser.setEmail(updateUserDTO.getEmail());
+        existingUser.setPassword(updateUserDTO.getPassword());
 
-                users.set(i, newUser);
-                return newUser;
-            }
-        }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        //existingUser = new User(id,updateUserDTO);
+
+        return userRepository.save(existingUser);
     }
 
 
